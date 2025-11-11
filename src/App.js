fc524a23898b6cc189ff94f14bc3c900eb088340
mxx <abdulramiz007@gmail.com>
2025-11-11 18:25:06 +0530

    Fix typo in h4 tag text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     
       <h1>✅ React Pipeline Test Successful! ramiz</h1>
       <p>This is a simple test build running through hhjg njk Azure DevOps.</p>
-       <h4>✅ React Pipeline Test Successul! ramiz</h4>
+       <h4>✅ React Pipeline Test Successul! ramghgjhiz</h4>
     </div>
   );
 }

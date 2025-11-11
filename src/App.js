8a18a9aff523727bb64a243ebfd5449e468f512f
mxx <abdulramiz007@gmail.com>
2025-11-11 13:30:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    
       <h1>✅ React Pipeline Test Successful! ramiz</h1>
       <p>This is a simple test build running through hhjg njk Azure DevOps.</p>
+       <h4>✅ React Pipeline Test Successul! ramiz</h4>
     </div>
   );
 }

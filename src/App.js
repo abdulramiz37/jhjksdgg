88b64a36ec6019308a6142eafdc78225cd75f5c9
mxx <abdulramiz007@gmail.com>
2025-11-12 13:20:29 +0530

    Fix typos in App.js content
    Corrected typos in paragraph and heading text.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
     <div className="App">
     
       <h1>✅ React Pipeline Test Successful! ramiz</h1>
-      <p>This is a simple test build running through hhjg njk Azure DevOps.</p>
-       <h4>✅ React Pipeline Test Successul! ramghgjhiz</h4>
+      <p>This is a simple test build running thdrough hhjg njk Azure DevOps.</p>
+       <h4>✅ React Pipeline Test Succesgfsul! ramghgjhiz</h4>
     </div>
   );
 }
